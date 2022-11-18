a3dbe32c76fdd445b43a55470472d7453e4ca7b9
Lucas Valadao <lukasfv10@gmail.com>
2022-11-18 18:05:24 -0300

    BoardException e programacao defensiva

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,9 @@ public class ChessMatch {
 
     private void initialSetup(){
         board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
+
+        board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
     }
+
 
 }
