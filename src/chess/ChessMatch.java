a6a054d8df7c41ada11b2e98b053496e7ccbb2f8
Lucas Valadao <lukasfv10@gmail.com>
2022-12-02 20:59:20 -0300

    Possiveis movimentos da torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -35,8 +35,9 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source,target);
         Piece capturedPiece = makeMove(source, target);
-        return (ChessPiece) capturedPiece;
+        return (ChessPiece)capturedPiece;
     }
 
     private Piece makeMove(Position source, Position target) {
@@ -52,6 +53,12 @@ public class ChessMatch {
         }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("Nao existe movimentos possiveis para a peca escolhida.");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("A peca escolhida nao pode se mover para posicao de destino");
         }
     }
 
@@ -72,6 +79,8 @@ public class ChessMatch {
 
 
     }
+
+
 
 
 }
